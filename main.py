c7e2f0571420283ca9537fc76c29a1af789bec7b
Israel Teneda <israteneda@gmail.com>
2022-03-18 18:08:58 -0500

    add more logic
    Co-authored-by: LuisMS7 <LuisMS7@users.noreply.github.com>
    Co-authored-by: Bryan Aguilar <bryanAguilar001@users.noreply.github.com>
    Co-authored-by: Francisco Navarro <f.navarro.ec@outlook.com>
    Co-authored-by: Rodrigo Farinango <rofarinango@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 """
 https://www.hackerrank.com/challenges/the-time-in-words/problem?isFullScreen=true
 """
-
+hours = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve"
+}
 
 def validate_hours_and_mins(hour: int, minutes: int):
     """
-    Validatet hours and minutes
+    Validate hours and minutes
     """
     return (1 <= hour <= 12) and (0 <= minutes < 60)
+
+def user_prompt():
+    hours = input()
+    minutes = input()
+    return (hours, minutes)
+
+def time_in_words(h,m):
+    return hours[h]
--- main_test.py
+++ main_test.py
@@ -1,5 +1,7 @@
 import pytest
-from main import validate_hours_and_mins
+import builtins
+from unittest import mock
+from main import validate_hours_and_mins, user_prompt, time_in_words
 
 @pytest.mark.parametrize(
     "hour, mins, expected_response",
@@ -12,3 +14,12 @@ def test_validate_entries__receive_hours_and_mins__return_bool(
     hour, mins, expected_response
 ):
     assert validate_hours_and_mins(hour, mins) == expected_response
+
+def test_validate_input():
+    mock_numbers = (x for x in [5, 25])
+    with mock.patch.object(builtins, 'input', side_effect = mock_numbers):
+        assert user_prompt() == (5, 25)
+
+def test_validate_entrie_receives_hours_equals_to_number_word():
+    assert time_in_words(5, None) == "five"
+    assert time_in_words(6, None) == "six"
